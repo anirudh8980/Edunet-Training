07464732b8d9dc05d4b74ee6a19191de707726c7
aniruh8980 <anirudhkumar5108@gmail.com>
2025-09-22 16:22:42 +0530

    commit

---
+++ predefined/predefined.js
@@ -0,0 +1,15 @@
+const a=Number(prompt ("enter a numer"));
+const b=Number(prompt ("enter a numer"));
+document.write("you entered a=",a ,"and b=",b);
+function add() {
+    alert(a+b);
+}
+function sub() {
+    alert(a-b);
+}
+function mul() {
+    alert(a*b);
+}
+function div() {
+    alert(a/b);
+}
