0b350cd0e5eb575c3f2f107aa62a9a8eea04b68c
aniruh8980 <anirudhkumar5108@gmail.com>
2025-09-22 14:15:27 +0530

    learn

--- Java Script/app.js
+++ Java Script/app.js
@@ -1,5 +1,39 @@
-var b = 749;
-var a = 947;
-console.log(a);
-console.log(a+b);
-console.log(`value of b is ${b}`);    
+// var b = 749;
+// var a = 947;
+// console.log(a);
+// console.log(a+b);
+// console.log(`value of b is ${b}`);    
+// console.log(typeof(a));
+// data = {
+//     name:"ani",
+//     age:"21",
+//     branch:"cse"
+// };
+// // console.log(typeof data);
+// function add() {
+//     console.log(typeof data);
+//     console.log(data);
+// }
+// add();
+// const del = () => {
+//     a =9;
+//     b =4;
+//     c =7;
+//     console.log(a+b+c);
+// }
+// del();
+
+// arr = [1,2,3]
+// console.log(arr);
+
+// const add = (a , b) => console.log(a+b);
+// add(50,40);
+
+// console.log(b);
+// b = 40;
+// let name = "ani";
+function demo() {
+    let name = "ani";
+   console.log(`My name is ${name}`);
+}
+demo();
